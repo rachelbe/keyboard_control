0b0a718ebf394d7e5e109cbe225aa7f78a6dde85
Rachel <67389036+rachelbe@users.noreply.github.com>
2020-06-30 18:11:09 -0400

    Add files via upload
    fixed entering the key so it moves without hitting enter

--- keyboard_input.py
+++ keyboard_input.py
@@ -22,26 +22,34 @@ def keyboard_input ():
 		
 		s =getch.getch()  #input to move 
 		if s == "w": #foward
-			twist.linear.x = 1*int(speed) 
+			twist.linear.x = -1*int(speed) 
 			twist.angular.z = 0
 			twist.linear.y = 0
 		elif s == "s":  #backward
-			twist.linear.x = -1*int(speed)
+			twist.linear.x = 1*int(speed)
 			twist.angular.z = 0
 			twist.linear.y = 0
-		elif s == "d": #right
+		elif s == "q": #foward and right
 			twist.linear.y = -1*int(speed)
-			twist.angular.z = 0
-			twist.linear.x = 0
-		elif s == "a": #left
+			twist.angular.z = -1*int(turn)
+			twist.linear.x = -1*int(speed)
+		elif s == "e": #foward and left
 			twist.linear.y = 1*int(speed)
-			twist.angular.z = 0
-			twist.linear.x = 0
-		elif s == "k": #turn right
+			twist.angular.z = 1*int(turn)
+			twist.linear.x = -1*int(speed)
+		elif s == "a": #backwards and right
+			twist.linear.y = -1*int(speed)
+			twist.angular.z = -1*int(turn)
+			twist.linear.x = 1*int(speed)
+		elif s == "d": #backwards and left
+			twist.linear.y = 1*int(speed)
+			twist.angular.z = 1*int(turn)
+			twist.linear.x = 1*int(speed)	
+		elif s == "k": #turn around right
 			twist.angular.z = 2*int(speed)
 			twist.linear.x = twist.linear.y = 0
-		elif s == "l": #turn left
-			twist.angular.z = -2*int(speed)
+		elif s == "l": #turn around left
+			twist.angular.z = -2*int(turn)
 			twist.linear.x = twist.linear.y = 0
 		elif s == "z": #quit
 			twist.angular.z = twist.linear.x = twist.linear.y = 0
