e416e06c7c7e7125ecac7047fdbd3a1aa3420f0b
Rachel <67389036+rachelbe@users.noreply.github.com>
2020-06-30 17:33:44 -0400

    Add files via upload

--- keyboard_input.py
+++ keyboard_input.py
@@ -4,6 +4,7 @@
 import rospy
 from geometry_msgs.msg import Twist
 import sys
+import getch
 
 twist = Twist()
 
@@ -12,43 +13,41 @@ def keyboard_input ():
 	pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
 	rospy.init_node('keyboard_input')
 	
-	
 	print("Let's move the robot!")
-	speed = input("Enter your speed:") #custom speed val
+	speed = input("Enter speed:") #custom speed val
 	turn = input ("Enter turn value: ") #custom turn val
 	
-	
-	
 	while not rospy.is_shutdown():
-		s = input(":- ")
-		if s == "w":
-			twist.linear.x = 1*int(speed)
+		
+		
+		s =getch.getch()  #input to move 
+		if s == "w": #foward
+			twist.linear.x = 1*int(speed) 
 			twist.angular.z = 0
 			twist.linear.y = 0
-		elif s == "s":
+		elif s == "s":  #backward
 			twist.linear.x = -1*int(speed)
 			twist.angular.z = 0
 			twist.linear.y = 0
-		elif s == "d":
+		elif s == "d": #right
 			twist.linear.y = -1*int(speed)
 			twist.angular.z = 0
 			twist.linear.x = 0
-		elif s == "a":
+		elif s == "a": #left
 			twist.linear.y = 1*int(speed)
 			twist.angular.z = 0
 			twist.linear.x = 0
-		elif s == "k":
+		elif s == "k": #turn right
 			twist.angular.z = 2*int(speed)
 			twist.linear.x = twist.linear.y = 0
-		elif s == "l":
+		elif s == "l": #turn left
 			twist.angular.z = -2*int(speed)
 			twist.linear.x = twist.linear.y = 0
-		elif s == "q":
+		elif s == "z": #quit
 			twist.angular.z = twist.linear.x = twist.linear.y = 0
 			sys.exit()
 		pub.publish(twist)
 		
-	
 	
 if __name__ == '__main__':
     try:
