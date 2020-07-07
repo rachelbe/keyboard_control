809653bd23d8a6235a286cc2c8f321e58dbbe419
Rachel <67389036+rachelbe@users.noreply.github.com>
2020-07-07 14:45:37 -0400

    Add files via upload

--- keyboard_input.py
+++ keyboard_input.py
@@ -8,6 +8,23 @@ import getch
 
 twist = Twist()
 
+keydir= """
+
+	Keys:
+	q - up and right
+	w - fowards 
+	e - up left
+	a - back right
+	s - backwards
+	d - back left
+
+	k -turn counter clockwise
+	l - turn clockwise
+
+	z - exit
+	-----------------
+"""
+
 def keyboard_input ():
 
 	pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
@@ -22,34 +39,34 @@ def keyboard_input ():
 		
 		s =getch.getch()  #input to move 
 		if s == "w": #foward
-			twist.linear.x = -1*int(speed) 
+			twist.linear.x = -1.0*float(speed) 
 			twist.angular.z = 0
 			twist.linear.y = 0
 		elif s == "s":  #backward
-			twist.linear.x = 1*int(speed)
+			twist.linear.x = 1.0*float(speed)
 			twist.angular.z = 0
 			twist.linear.y = 0
 		elif s == "q": #foward and right
-			twist.linear.y = -1*int(speed)
-			twist.angular.z = -1*int(turn)
-			twist.linear.x = -1*int(speed)
+			twist.linear.y = -1.0*float(speed)
+			twist.angular.z = 1.0*float(turn)
+			twist.linear.x = -1.0*float(speed)
 		elif s == "e": #foward and left
-			twist.linear.y = 1*int(speed)
-			twist.angular.z = 1*int(turn)
-			twist.linear.x = -1*int(speed)
+			twist.linear.y = 1.0*float(speed)
+			twist.angular.z = -1.0*float(turn)
+			twist.linear.x = -1.0*float(speed)
 		elif s == "a": #backwards and right
-			twist.linear.y = -1*int(speed)
-			twist.angular.z = -1*int(turn)
-			twist.linear.x = 1*int(speed)
+			twist.linear.y = -1.0*float(speed)
+			twist.angular.z = -1.0*float(turn)
+			twist.linear.x = 1.0*float(speed)
 		elif s == "d": #backwards and left
-			twist.linear.y = 1*int(speed)
-			twist.angular.z = 1*int(turn)
-			twist.linear.x = 1*int(speed)	
+			twist.linear.y = 1.0*float(speed)
+			twist.angular.z = 1.0*float(turn)
+			twist.linear.x = 1.0*float(speed)	
 		elif s == "k": #turn around right
-			twist.angular.z = 2*int(speed)
+			twist.angular.z = 2.0*float(speed)
 			twist.linear.x = twist.linear.y = 0
 		elif s == "l": #turn around left
-			twist.angular.z = -2*int(turn)
+			twist.angular.z = -2.0*float(turn)
 			twist.linear.x = twist.linear.y = 0
 		elif s == "z": #quit
 			twist.angular.z = twist.linear.x = twist.linear.y = 0
@@ -59,5 +76,11 @@ def keyboard_input ():
 	
 if __name__ == '__main__':
     try:
-        keyboard_input()
+    	print(keydir)
+    	keyboard_input()
     except rospy.ROSInterruptException:pass
+    
+    
+    
+    
+    
